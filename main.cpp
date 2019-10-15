8362b43553b78529f27d584f14354ee0c13bd647
Stephano <stephano.wurttele@utec.edu.pe>
2019-10-15 00:47:04 -0500

    Terminado

--- main.cpp
+++ main.cpp
@@ -1,13 +1,42 @@
-#include <cstdio>
+#include <iostream>
+
 using namespace std;
+
+//int ans[13] = { 2, 7, 5, 30, 169, 441, 1872, 7632, 1740, 93313, 459901,1358657, 2504881 };
+ int ans[13] = { 0, 0, 0,  0,   0,   0,    0,    0,    0,     0,      0,      0,       0 };
+
+
+void calcular(int k){
+	int saltos=k;
+	int temp=1;
+	while (true){
+		int killed=0;
+		int personas=2*k;
+		while(personas!=k){
+			killed=(killed+saltos)%personas;
+			if(killed<k)
+				break;
+			personas--;
+		}
+		if(personas==k){
+			ans[k-1]=saltos;
+			return;
+		}
+		saltos++;
+		while((saltos+1%(2*k))<=k)
+			saltos++;
+	}
+
+}
+
+
 
 int main() {
 		int k;
-			int ans[13] = { 2, 7, 5, 30, 169, 441, 1872, 7632, 1740, 93313, 459901,
-							1358657, 2504881 };
-
-				while (scanf("%d", &k), k) {
-							printf("%d\n",ans[k-1]);
-				}
-				return 0;
+		while (scanf("%d", &k), k) {
+			if(ans[k-1]==0)
+				calcular(k);
+			printf("%d\n",ans[k-1]);
+		}
+		return 0;
 }
